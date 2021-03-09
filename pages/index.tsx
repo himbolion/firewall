b69b00214e869cf1bab1a60bc0e7619f9fd88e9c
Lio <lio@werewolf.design>
2021-03-09 02:47:19 +0100

    :sparkles: make landing page for new portfolio

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,20 @@
 import Link from 'next/link'
 import Layout from '../components/Layout'
 
+
 const IndexPage = () => (
-  <Layout title="Home | Next.js + TypeScript Example">
-    <h1>Hello Next.js 👋</h1>
-    <p>
-      <Link href="/about">
-        <a>About</a>
+  <Layout title="Home">
+    <h1 className="child">
+      <Link href="https://github.com/hokkqi/firewall">
+        <a >Firewall</a>
       </Link>
-    </p>
+      {" "}
+      by
+      {" "}
+      <Link href="https://werewolf.computer">
+        <a >Lio</a>
+      </Link>
+    </h1>
   </Layout>
 )
 
